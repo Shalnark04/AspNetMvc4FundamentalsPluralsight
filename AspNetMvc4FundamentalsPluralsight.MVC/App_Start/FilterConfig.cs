8fc3bade07147e8c6f82eddde29ef7ac12b3cc7e
LucasBebenek <lucas@PersonalWebZone.com>
2015-09-19 11:14:11 -0700

    Initroduced an initial MVC application project into the solution

---
+++ AspNetMvc4FundamentalsPluralsight.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc4FundamentalsPluralsight.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspNetMvc4FundamentalsPluralsight.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvc4FundamentalsPluralsight.MVC.MvcApplication" Language="C#" %>

381ff52e9a64e7ab1774079c8d082299fb72f01c
LucasBebenek <lucas@PersonalWebZone.com>
2015-09-25 07:21:15 -0700

    Created Video Model class and VideoDb

---
+++ AspNetMvc4FundamentalsPluralsight.WebApi/Models/Video.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvc4FundamentalsPluralsight.WebApi.Models
+{
+    public class Video
+    {
+        public virtual int VideoId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual int Length { get; set; }
+    }
+}

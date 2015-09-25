a2073b382f0ce90874ef531ad83127a602630bee
LucasBebenek <lucas@PersonalWebZone.com>
2015-09-25 07:17:17 -0700

    Created Video Controller

---
+++ AspNetMvc4FundamentalsPluralsight.WebApi/Controllers/VideosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetMvc4FundamentalsPluralsight.WebApi.Controllers
+{
+    public class VideosController : ApiController
+    {
+        // GET: api/Videos
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Videos/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Videos
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Videos/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Videos/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
